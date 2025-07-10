0409610ee3d5da6d0b27f6345b3d02a1fd99a93a
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2025-07-10 19:27:44 -0300

    feat: Implement ConfirmTransactionDeletion modal for transaction removal

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,33 +1,91 @@
-import { ITransactionResponse } from "@/types/transaction";
+import { ITransaction } from "@/types/transaction";
 import { formatCurrency, formatDate } from "@/utils";
+import { ConfirmTransactionDeletion } from "../Modal/ConfirmTransactionDeletion";
+import { useState } from "react";
 
 export interface ITableProps {
-    transactions: ITransactionResponse
+  transactions: ITransaction[] | undefined;
 }
 
-export function Table({transactions}: ITableProps) {   
-    return (  
-        <>     
-        <table className="w-full mt-16 border-0 border-separate border-spacing-y-2 ">
+export function Table({ transactions }: ITableProps) {
+  const [transactionToBeDeleted, setTransactionToBeDeleted] =
+    useState<ITransaction | null>(null);
+
+  if (!transactions || transactions.length === 0) {
+    return (
+      <div className="text-center text-gray-500">
+        Nenhuma transação encontrada.
+      </div>
+    );
+  }
+
+  const handleOpenDeleteTransactionModal = (transaction: ITransaction) => {
+    setTransactionToBeDeleted(transaction);
+  };
+
+  const handleCloseDeleteTransactionModal = () => {
+    setTransactionToBeDeleted(null);
+  }
+
+  return (
+    <>
+      <table className="w-full mt-4 border-0 border-separate border-spacing-y-2 ">
         <thead>
-            <tr>
-                <th className="px-4 text-left text-table-header text-base font-medium">Título</th>
-                <th className="px-4 text-left text-table-header text-base font-medium">Preço</th>
-                <th className="px-4 text-left text-table-header text-base font-medium">Categoria</th>
-                <th className="px-4 text-left text-table-header text-base font-medium">Data</th>                                   
-            </tr>
+          <tr>
+            <th className="px-4 text-left text-table-header text-base font-medium">
+              Título
+            </th>
+            <th className="px-4 text-left text-table-header text-base font-medium">
+              Preço
+            </th>
+            <th className="px-4 text-left text-table-header text-base font-medium">
+              Categoria
+            </th>
+            <th className="px-4 text-left text-table-header text-base font-medium">
+              Data
+            </th>
+            <th className="px-4 text-left text-table-header text-base font-medium">
+              Ações
+            </th>
+          </tr>
         </thead>
         <tbody>
-            {transactions.data.map((transaction, index) => (
-                <tr key={index} className="bg-white h-16 rounded-lg">
-                    <td className="px-4 py-4 whitespace-nowrap text-title">{transaction.title}</td>
-                    <td className={`px-4 py-4 whitespace-nowrap text-right ${transaction.type === 'INCOME'? "text-income" : "text-outcome"}`}>{formatCurrency(transaction.price)}</td>
-                    <td className="px-4 py-4 whitespace-nowrap text-table">{transaction.category}</td>
-                    <td className="px-4 py-4 whitespace-nowrap text-table">{transaction.data ? formatDate(new Date(transaction.data)) : ''}</td>                             
-                </tr>
-            ))}
+          {transactions.map((transaction, index) => (
+            <tr key={index} className="bg-white h-16 rounded-lg">
+              <td className="px-4 py-4 whitespace-nowrap text-title">
+                {transaction.title}
+              </td>
+              <td
+                className={`px-4 py-4 whitespace-nowrap text-right ${
+                  transaction.type === "INCOME" ? "text-income" : "text-outcome"
+                }`}
+              >
+                {formatCurrency(transaction.price)}
+              </td>
+              <td className="px-4 py-4 whitespace-nowrap text-table">
+                {transaction.category}
+              </td>
+              <td className="px-4 py-4 whitespace-nowrap text-table">
+                {transaction.data ? formatDate(new Date(transaction.data)) : ""}
+              </td>
+              <td className="px-4 py-4 whitespace-nowrap">
+                <button className="text-blue-500 hover:underline cursor-pointer">
+                  Editar
+                </button>
+                <button
+                  onClick={() => handleOpenDeleteTransactionModal(transaction)}
+                  className="text-red-500 hover:underline ml-4 cursor-pointer"
+                >
+                  Excluir
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
-    </table>    
-    </> 
-    )
+      </table>
+      {transactionToBeDeleted && (
+        <ConfirmTransactionDeletion transaction={transactionToBeDeleted} handleCloseDeleteTransactionModal={handleCloseDeleteTransactionModal} />
+      )}
+    </>
+  );
 }
